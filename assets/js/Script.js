014a6011187871794c8365368d98acb3b574cd25
Virgil <Virgilius.zip@gmail.com>
2023-02-02 01:12:12 -0500

    jnlkm;ASD

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -3,9 +3,18 @@ const apikey = "6fb2b354c822933f6fd35ad30af473fe"
 
 let order = 1
 
-
 onload = function LoadForecast() {
 
+    let lon = "39.9527237";
+
+    let lat = "-75.1635262"; 
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+apikey+"&units=imperial&cnt=5&exclude=hourly,minutelyalerts")
+.then(response => response.json())
+    .then(data => {
+
+        console.log(data)
+    })
 };
 
 onload = function LoadCities() {
@@ -13,26 +22,66 @@ onload = function LoadCities() {
 };
 
 
-function CitySearch()
+function CitySearch(cityinput)
 {
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityinput+"&limit=5&appid="+apikey+"&units=imperial")
+    console.log("Hi")
+
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q="+cityinput+"&limit=5&appid="+apikey+"&units=imperial")
     .then(response => response.json())
     .then(data => {
 
-    var location = data.name[0];
-    console.log(location);
+        
 
-    var latitude = (data["coord"]["lat"]);
-    console.log(latitude);
+    var place = data[0].name;
+    console.log(place);
 
-    var longitude = (data["coord"]["lon"]);
-    console.log(longitude);
+    var latitude = data[0].lat;
+    console.log("Lat:"+latitude);
 
-    
+    var longitude = data[0]["lon"];
+    console.log("Lon:"+longitude);
 
+    CurrentWeatherSearch(latitude,longitude)
     })
 
-    .catch(error => alert("Wrong"))
+    .catch(error => 
+        {console.error("Wrong")
+
+    console.log(error)
+    })
+
+}
+
+function CurrentWeatherSearch(latitude,longitude){
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+apikey+"&units=imperial")
+    .then(response => response.json())
+    .then(data =>{
+    
+        
+     var location = data["name"];
+     console.log(location)
+
+     var temp = "Temp: "+(data["main"]["temp"])+"°F";
+     console.log(temp)
+
+     var wind = "Wind Speed: "+(data["wind"]["speed"])+" MPH";
+     console.log(wind)
+
+     var humidity = "Humidity: "+(data["main"]["humidity"])+"%";
+     console.log(humidity)
+    
+
+
+    console.log(longitude)
+    console.log(latitude)
+
+    $(".CityDisplay > .City").text(location);
+    $(".CityDisplay > .Temp").text(temp);
+    $(".CityDisplay > .Wind").text(wind);
+    $(".CityDisplay > .Humidity").text(humidity);
+
+    })
 
 }
 
@@ -42,7 +91,7 @@ $(document).ready(function () {
     // saveBtn click listener 
     $(".searchbutton").on("click", function () {
         // Get nearby values of the description in JQuery
-        console.log ("Searching For City Weather")
+        console.log ("Taco Searching For City Weather")
 
         var cityinput = $(this).siblings("#cityinput").val();
 
@@ -66,7 +115,8 @@ $(document).ready(function () {
         order = 1
         } 
 
-        CitySearch();
+
+        CitySearch(cityinput);
 
 
     }
